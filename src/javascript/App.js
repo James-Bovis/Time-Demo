20d00f0f697fee86ec3bccad6f795e2ca55c463b
James-Bovis <hello@jamesbovis.com>
2019-09-11 08:53:21 +0100

    Generate a background Gradient depending on the current hour
    This commit adds a new feature of which a different background gradient is
    rendered depending on the time of day. Currently there are three different
    gradients for Morning, Afternoon and Evening.

--- src/javascript/App.js
+++ src/javascript/App.js
@@ -1,17 +1,33 @@
 import React, { Suspense } from 'react'
+import moment from 'moment'
 import Clock from './components/Clock'
 import { Gradient } from 'react-gradient'
 import '../assets/stylesheets/App.sass'
 
-const gradients = [
-  ['#0F2027', '#203A43'],
-  ['#2C5364', '#1E5D8A']
-]
+const hour = moment().format('HH')
+
+const genereateGradients = (hour): Array => {
+  if (hour < '12') {
+    return (
+      [['#1a2a6c', '#b21f1f', '#fdbb2d']]
+    )
+  }
+  else if (hour >= '19') {
+    return (
+      [['#000000', '#434343', '#434343', '#000000']]
+    )
+  }
+  else if (hour <='19') {
+    return (
+      [['#2980b9', '#2c3e50', '#2c3e50', '#2980b9', ]]
+    )
+  }
+}
 
 const App = () => {
   return (
       <Gradient
-        gradients={gradients}
+        gradients={genereateGradients(hour)}
         property='background'
         duration={5000}
         angle='45deg'
